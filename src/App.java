7e4210c4a7317e27849f9ac93d3920e0dce325bf
DK2801 <khanh2812000123@gmail.com>
2022-06-12 17:57:29 +0700

    demo

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+Scanner in = new Scanner(System.in);
+String s = in.nextLine();
+System.out.println(s);
+    }
+}
